fd48431bc02fc43de2325de7f9a3089f321d36c5
Ayushmaan Shandilya <90828689+ayushmaanxs@users.noreply.github.com>
2024-02-11 14:06:01 +0530

    Valentine Special

---
+++ Practice/Love Click/script.js
@@ -0,0 +1,33 @@
+const wrapper = document.querySelector('.wrapper');
+const question = document.querySelector('.question');
+const yesBtn = document.querySelector('.yes-btn');
+const noBtn = document.querySelector('.no-btn');
+const wrapperRect = wrapper.getBoundingClientRect();
+const noBtnRect = noBtn.getBoundingClientRect();
+yesBtn.addEventListener('click', () => {
+    question.innerHTML = 'I Love You Too :)';
+});
+noBtn.addEventListener('mouseover', () => {
+    const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
+    const j = Math.floor(Math.random() * (wrapperRect.height - noBtnRect.height)) + 1;
+    noBtn.style.left = i + 'px';
+    noBtn.style.top = j + 'px';
+});
+
+const changeText = document.querySelector("#change-text");
+
+changeText.addEventListener("click", function() {
+  changeText.textContent = "Love you too:)";
+});
+
+function afterr(){
+    document.getElementById('Myimage')
+    .src="love.jpg";
+};
+
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+  // 👇️ hide button
+  btnx.style.visibility = 'hidden';
+});
--- README.md
+++ README.md
@@ -2,8 +2,7 @@
 All the codes which I have done during learning web development
 
 ##Demo Projects from this repo
-
-###[Portfolio Website](https://portfolio-ayushmaanxs.netlify.app)
-###[Parallex Website](https://parallax-ayushmaanxs.netlify.app/)
-###[Netflix Clone](https://netlfix-ayushmaanxs.netlify.app)
-###[Image Gallery](https://image-gallery-ayushmaanxs.netlify.app)
+[**Portfolio Website**](https://portfolio-ayushmaanxs.netlify.app)
+[**Parallex Website**](https://parallax-ayushmaanxs.netlify.app/)
+[**Netflix Clone**](https://netlfix-ayushmaanxs.netlify.app)
+[**Image Gallery**](https://image-gallery-ayushmaanxs.netlify.app)
